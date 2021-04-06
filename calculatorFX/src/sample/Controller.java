c66239cc1294795e31adf5601b942f9fd4be97f5
Chawapon <chawapon2001@gmail.com>
2021-04-06 16:09:05 +0700

    update css color

--- calculatorFX/src/sample/Controller.java
+++ calculatorFX/src/sample/Controller.java
@@ -15,30 +15,89 @@ import java.util.ResourceBundle;
 public class Controller {
     @FXML
     private Label label;
-    private long number1;
-    private long number2;
+    private float number1;
+    private float number2;
+    private float   output=0;
+    private boolean getOut=false;
     private String  operator="";
-    private boolean start=true;
+    private boolean start=false;
     Model   model=new Model();
     @FXML
-    private void processNumbers(ActionEvent event) {
-        if(start==true){
-            label.setText("");
+    public void processClear(ActionEvent event) {
+        String  input=((Button)event.getSource()).getText();
+        if(input.equals("OFF")){
             start=false;
+            label.setText("");
+            number1=0;
+            number2=0;
+            operator="";
         }
-        String value=((Button)event.getSource()).getText();
-        label.setText(label.getText()+value);
+        else if(input.equals("C")){
+            label.setText("0");
+            number1=0;
+            number2=0;
+            operator="";
+            start=false;
+
+        }
+        else if(input.equals("CE")){
+            label.setText("0");
+            number1=0;
+            number2=0;
+            operator="";
+        }
+
+    }
+    @FXML
+    public void processChange(ActionEvent event) {
+        String  input=((Button)event.getSource()).getText();
+        float   temp=Float.parseFloat(label.getText());
+        if(input.equals("+/-")){
+            if(temp>0 || temp<0){
+               temp= model.calculate(temp,-1,"*");
+                label.setText(String.valueOf(temp));
+            }
+            else {
+                label.setText(String.valueOf(temp));
+            }
+
+        }
+        else if(input.equals("√")){
+            temp= model.calculate(temp,1,"√");
+            label.setText(String.valueOf(temp));
+        }
+        else if(input.equals("%")){
+            temp= model.calculate(temp,1,"%");
+            label.setText(String.valueOf(temp));
+        }
+        else if(input.equals("1/x")){
+            temp= model.calculate(temp,1,"1/x");
+            label.setText(String.valueOf(temp));
+        }
+
+
+    }
+
+    @FXML
+    private void processNumbers(ActionEvent event) {
+        if(start==false){
+            label.setText("");
+            start=true;
+        }
+            String value = ((Button) event.getSource()).getText();
+            label.setText(label.getText() + value);
     }
 
     @FXML
     private void processOperators(ActionEvent event) {
         String  value=((Button)event.getSource()).getText();
         if(value.equals("=")==false){
-            if(!operator.equals("")){
+            if(operator.equals("")==false){
                 return;
             }
             operator=value;
-            number1=Long.parseLong(label.getText());
+
+            number1=Float.parseFloat(label.getText());
             label.setText("");
 
         }
@@ -46,40 +105,38 @@ public class Controller {
             if(operator.equals("")){
                 return;
             }
-            number2=Long.parseLong(label.getText());
-            calculate(number1,number2,operator);
+            number2=Float.parseFloat(label.getText());
+           output= model.calculate(number1,number2,operator);
+            //getOut=true;
+            label.setText(String.valueOf(output));
             operator="";
         }
 
     }
 
-    public void calculate(long n1,long n2,String operator)
+    public long calculate(long n1,long n2,String operator)
     {
         switch (operator)
         {
             case"+":
-                label.setText(n1+n2+"");
-                break;
+                return n1+n2;
+
             case "-":
-                label.setText(n1-n2+"");
-                break;
+                return n1-n2;
             case "/":
                 if (n2==0){
-                    label.setText("0");
-                    break;
+                    return 0;//
                 }
                 else {
-                    label.setText(n1/n2 + "");
-                    break;
+                    return n1/n2;
                 }
             case"*":
-                label.setText(n1*n2+"");
-                break;
+                return n1*n2;
             default:
-                break;
-
+                return 0;
         }
     }
+
 
 
 }
--- calculatorFX/src/sample/Model.java
+++ calculatorFX/src/sample/Model.java
@@ -1,5 +1,6 @@
 package sample;
 
+import java.lang.Math;
 public class Model {
     public Model() {
     }
@@ -7,7 +8,7 @@ public class Model {
 
     }
 
-    public float calculate(long n1, long n2, String operator)
+    public float calculate(float n1, float n2, String operator)
     {
         switch (operator)
         {
@@ -22,6 +23,12 @@ public class Model {
                 return  n1/n2;
             case"*":
                 return n1*n2;
+            case"√":
+                return (float) Math.sqrt(n1);
+            case"%":
+                return (float) n1/100;
+            case"1/x":
+                return (float) 1/n1;
             default:
                 return  0;
 
