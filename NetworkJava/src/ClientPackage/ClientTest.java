334eb929afd9c9a3e8c6a52c1ac1c67f7efd91f4
Chawapon <chawapon2001@gmail.com>
2021-04-10 22:13:26 +0700

    socket network JFRAME

---
+++ NetworkJava/src/ClientPackage/ClientTest.java
@@ -0,0 +1,13 @@
+package ClientPackage;
+
+import ServerPackage.*;
+
+import javax.swing.*;
+
+public class ClientTest {
+    public static void main(String[] args) {
+        Client  client1=new Client("localhost");
+        client1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        client1.startRunning();
+    }
+}
