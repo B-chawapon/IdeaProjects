15db5b8e783a3c026544138c26cbccc1ad9c0e37
Chawapon <chawapon2001@gmail.com>
2021-05-02 03:39:20 +0700

    Update AftermatchController.java

--- slavegameParn/src/sample/AftermatchController.java
+++ slavegameParn/src/sample/AftermatchController.java
@@ -1,10 +1,17 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -18,6 +25,21 @@ public class AftermatchController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }
+    @FXML
+    public static void display() throws IOException {
+        Parent alertParent = FXMLLoader.load(SoundController.class.getResource("aftermatch.fxml"));
+        Scene alertScene = new Scene(alertParent);
+        //alertScene.getStylesheets().add("styles/style.css");
+
+        Stage window1 = new Stage();
+        window1.initModality(Modality.APPLICATION_MODAL);
+        window1.setTitle("End");
+        // window1.getIcons().add(new Image("images/icon.png"));
+
+        window1.setScene(alertScene);
+        window1.showAndWait();
+    }
+
     @FXML
     private ImageView PlayagainButton;
     @FXML
@@ -45,6 +67,8 @@ public class AftermatchController implements Initializable {
 
     @FXML
     private void Gobackbutton(MouseEvent event) {
+        Stage stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
+        stage.close();
     }
 
 
